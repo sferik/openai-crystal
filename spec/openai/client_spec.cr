f0fd2deb31100c33bbf6f4a5473682f7d03a9ad1
Moisès Macià <mmacia@gmail.com>
2023-03-27 09:41:41 +0200

    fix completions

--- spec/openai/client_spec.cr
+++ spec/openai/client_spec.cr
@@ -2,50 +2,50 @@ require "./../spec_helper"
 
 describe OpenAI::Client do
   describe "Unauthorized" do
-    WebMock.stub(:get, "https://api.openai.com/v1/engines/davinci-v2")
+    WebMock.stub(:get, "https://api.openai.com/v1/models/davinci-v2")
       .with(headers: request_headers)
       .to_return(body: File.read("spec/fixtures/errors/unauthorized.json"), headers: response_headers, status: 401)
 
     it "raises an exception" do
       expect_raises(OpenAI::Client::Error, "Incorrect API key provided: sk-f00. You can find your API key at https://beta.openai.com.") do
         with_client do |client|
-          client.engine("davinci-v2")
+          client.model("davinci-v2")
         end
       end
     end
   end
 
   describe "Not Found" do
-    WebMock.stub(:get, "https://api.openai.com/v1/engines/turing")
+    WebMock.stub(:get, "https://api.openai.com/v1/models/turing")
       .with(headers: request_headers)
       .to_return(body: File.read("spec/fixtures/errors/not_found.json"), headers: response_headers, status: 404)
 
     it "raises an exception" do
       expect_raises(OpenAI::Client::Error, "No engine with that ID: turing") do
         with_client do |client|
-          client.engine("turing")
+          client.model("turing")
         end
       end
     end
   end
 
-  describe "#default_engine" do
-    it "returns the default engine" do
+  describe "#default_model" do
+    it "returns the default model" do
       with_client do |client|
-        client.default_engine.should eq("davinci")
+        client.default_model.should eq("davinci")
       end
     end
   end
 
   describe "#last_response" do
-    WebMock.stub(:get, "https://api.openai.com/v1/engines/davinci")
+    WebMock.stub(:get, "https://api.openai.com/v1/models/davinci")
       .with(headers: request_headers)
-      .to_return(body: File.read("spec/fixtures/engine.json"), headers: response_headers)
+      .to_return(body: File.read("spec/fixtures/model.json"), headers: response_headers)
 
     it "returns the last response" do
       with_client do |client|
         client.last_response.should be_nil
-        client.engine("davinci")
+        client.model("davinci")
         last_response = client.last_response
         last_response.should be_a(OpenAI::Client::LastResponse)
         last_response.not_nil!.organization.should be_a(String)
@@ -58,36 +58,36 @@ describe OpenAI::Client do
     end
   end
 
-  describe "#engine" do
-    WebMock.stub(:get, "https://api.openai.com/v1/engines/davinci")
+  describe "#model" do
+    WebMock.stub(:get, "https://api.openai.com/v1/models/davinci")
       .with(headers: request_headers)
-      .to_return(body: File.read("spec/fixtures/engine.json"), headers: response_headers)
+      .to_return(body: File.read("spec/fixtures/model.json"), headers: response_headers)
 
     it "retrieves an engine" do
       with_client do |client|
-        engine = client.engine("davinci")
-        engine.id.should eq("davinci")
-        engine.owner.should eq("openai")
-        engine.ready.should be_a(Bool)
+        model = client.model("davinci")
+        model.id.should eq("davinci")
+        model.owned_by.should eq("openai")
+        model.root.should eq("davinci")
       end
     end
   end
 
-  describe "#engines" do
-    WebMock.stub(:get, "https://api.openai.com/v1/engines")
+  describe "#models" do
+    WebMock.stub(:get, "https://api.openai.com/v1/models")
       .with(headers: request_headers)
-      .to_return(body: File.read("spec/fixtures/engines.json"), headers: response_headers)
+      .to_return(body: File.read("spec/fixtures/models.json"), headers: response_headers)
 
-    it "lists engines" do
+    it "lists models" do
       with_client do |client|
-        engines = client.engines
-        engines.should be_a(Array(OpenAI::Engine))
+        models = client.models
+        models.should be_a(Array(OpenAI::Model))
       end
     end
   end
 
   describe "#completions" do
-    WebMock.stub(:post, "https://api.openai.com/v1/engines/ada/completions")
+    WebMock.stub(:post, "https://api.openai.com/v1/completions")
       .with(headers: request_headers)
       .to_return(body: File.read("spec/fixtures/completions.json"), headers: response_headers)
 
